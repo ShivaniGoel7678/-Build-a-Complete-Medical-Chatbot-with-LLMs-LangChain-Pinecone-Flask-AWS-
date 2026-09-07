5e83c5dda0a94cdf8a8dd4f532b785fff0f9ea79
shivani <sg6131021@gmail.com>
2026-09-07 15:23:14 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages ,setup
+setup(
+    
+    name="medical chatbot"
+    version="0.1.0",
+    author="shivani goel",
+    author_email="sg6131021@gmail.com"
+    packages=find_packages(),
+    install_requires=[]
+    
+)
